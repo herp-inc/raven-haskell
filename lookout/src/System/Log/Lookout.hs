9752d06a0eb53b89cbe188956c8681f9c67772be
wiz <aenor.realm@gmail.com>
2013-02-26 15:43:09 +0400

    reshuffle functions to match export order

--- lookout/src/System/Log/Lookout.hs
+++ lookout/src/System/Log/Lookout.hs
@@ -54,20 +54,39 @@ import qualified Control.Exception as E
 
 import System.Log.Lookout.Types
 
--- | Record an event using logging service.
-record :: String                         -- ^ Logger name.
-       -> SentryLevel                    -- ^ Level
-       -> String                         -- ^ Message
-       -> (SentryRecord -> SentryRecord) -- ^ Additional options
-       -> IO SentryRecord
-record logger lvl msg upd = do
-    eid <- (filter (/= '-') . show) `fmap` (randomIO :: IO UUID)
-    ts <- formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%Q" `fmap` getCurrentTime
-    return $! upd (newRecord eid msg ts lvl logger)
+-- | Initialize event service.
+initLookout :: String                                    -- ^ Sentry DSN
+            -> (SentryRecord -> SentryRecord)            -- ^ Default fields updater. Use 'id' if not needed.
+            -> (SentrySettings -> SentryRecord -> IO ()) -- ^ Event transport from Looklout.Transport.*
+            -> (SentryRecord -> IO ())                   -- ^ Fallback handler.
+            -> IO SentryService                          -- ^ Event service to use in 'register'.
+initLookout dsn d t fb = return
+    SentryService { serviceSettings = fromDSN dsn
+                  , serviceDefaults = d
+                  , serviceTransport = t
+                  , serviceFallback = fb
+                  }
 
--- | JSON-encode record data.
-recordLBS :: SentryRecord -> ByteString
-recordLBS = encode
+-- | Disabled service that ignores incoming events.
+disabledLookout :: IO SentryService
+disabledLookout = initLookout "" id undefined undefined
+
+-- | Ask service to store an event.
+register :: SentryService                  -- ^ Configured lookout service.
+         -> String                         -- ^ Logger name.
+         -> SentryLevel                    -- ^ Sentry event level.
+         -> String                         -- ^ Message.
+         -> (SentryRecord -> SentryRecord) -- ^ Record updates.
+         -> IO ()
+register s loggerName level message upd = do
+    rec <- record loggerName level message (upd . serviceDefaults s)
+
+    let transport = serviceTransport s
+
+    case serviceSettings s of
+        SentryDisabled -> return ()
+        settings -> E.catch (transport settings rec)
+                            (\(E.SomeException _) -> serviceFallback s $ rec)
 
 -- | Show basic message on stderr.
 stderrFallback :: SentryRecord -> IO ()
@@ -87,36 +106,17 @@ errorFallback rec = error $ "Error sending record: " ++ show rec
 silentFallback :: SentryRecord -> IO ()
 silentFallback _ = return ()
 
--- | Initialize event service.
-initLookout :: String                                    -- ^ Sentry DSN
-            -> (SentryRecord -> SentryRecord)            -- ^ Default fields updater. Use 'id' if not needed.
-            -> (SentrySettings -> SentryRecord -> IO ()) -- ^ Event transport from Looklout.Transport.*
-            -> (SentryRecord -> IO ())                   -- ^ Fallback handler.
-            -> IO SentryService                          -- ^ Event service to use in 'register'.
-initLookout dsn d t fb = return
-    SentryService { serviceSettings = fromDSN dsn
-                  , serviceDefaults = d
-                  , serviceTransport = t
-                  , serviceFallback = fb
-                  }
+-- | Record an event using logging service.
+record :: String                         -- ^ Logger name.
+       -> SentryLevel                    -- ^ Level
+       -> String                         -- ^ Message
+       -> (SentryRecord -> SentryRecord) -- ^ Additional options
+       -> IO SentryRecord
+record logger lvl msg upd = do
+    eid <- (filter (/= '-') . show) `fmap` (randomIO :: IO UUID)
+    ts <- formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%Q" `fmap` getCurrentTime
+    return $! upd (newRecord eid msg ts lvl logger)
 
--- | Disabled service that ignores incoming events.
-disabledLookout :: IO SentryService
-disabledLookout = initLookout "" id undefined undefined
-
--- | Ask service to store an event.
-register :: SentryService
-         -> String
-         -> SentryLevel
-         -> String
-         -> (SentryRecord -> SentryRecord)
-         -> IO ()
-register s loggerName level message upd = do
-    rec <- record loggerName level message (upd . serviceDefaults s)
-
-    let transport = serviceTransport s
-
-    case serviceSettings s of
-        SentryDisabled -> return ()
-        settings -> E.catch (transport settings rec)
-                            (\(E.SomeException _) -> serviceFallback s $ rec)
+-- | JSON-encode record data.
+recordLBS :: SentryRecord -> ByteString
+recordLBS = encode
