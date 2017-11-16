838c38e20e19d8cfbb7c9423c184de89b522cc3e
Alexander Bondarenko <aenor.realm@gmail.com>
2017-11-16 21:10:57 +0300

    Add `sendRecordWith` HTTP transport variant
    It uses external HTTP Manager to save resources.
    
    (Fixes #1)

--- README.md
+++ README.md
@@ -2,10 +2,11 @@ Raven is a client for Sentry event server (<https://www.getsentry.com/>).
 
 Start by initializing the raven 'Service':
 
+    http <- newManager tlsManagerSettings
     l <- initRaven
           "https://pub:priv@sentry.hostname.tld:8443/sentry/example_project"
           id
-          sendRecord
+          (sendRecordWith http)
           stderrFallback
 
 Send events using 'register' function:
--- raven-haskell/src/System/Log/Raven/Transport/HttpConduit.hs
+++ raven-haskell/src/System/Log/Raven/Transport/HttpConduit.hs
@@ -3,9 +3,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module System.Log.Raven.Transport.HttpConduit
     ( sendRecord
+    , sendRecordWith
     ) where
 
-import Network (withSocketsDo)
+import Control.Monad.Trans (MonadIO, liftIO)
+import Control.Monad.Trans.Resource (runResourceT)
 import Network.HTTP.Conduit
 import qualified Data.ByteString.Char8 as BS
 
@@ -13,7 +15,12 @@ import System.Log.Raven
 import System.Log.Raven.Types
 
 sendRecord :: SentrySettings -> SentryRecord -> IO ()
-sendRecord conf rec = withSocketsDo $ do
+sendRecord conf rec = do
+    manager <- newManager tlsManagerSettings
+    runResourceT $ sendRecordWith manager conf rec
+
+sendRecordWith :: MonadIO m => Manager -> SentrySettings -> SentryRecord -> m ()
+sendRecordWith manager conf rec = do
     let ep = endpointURL conf
     let auth = concat [ "Sentry sentry_version=2.0"
                       , ", sentry_client=raven-haskell-0.1.0.0"
@@ -23,10 +30,10 @@ sendRecord conf rec = withSocketsDo $ do
     case ep of
         Nothing -> return ()
         Just url -> do
-            req' <- parseUrl url
+            req' <- liftIO $ parseUrlThrow url
             let req = req' { method = "POST"
                            , requestHeaders = [("X-Sentry-Auth", BS.pack auth)]
                            , requestBody = RequestBodyLBS (recordLBS rec)
                            }
-            res <- withManager $ httpLbs req
+            _ <- httpLbs req manager
             return ()
